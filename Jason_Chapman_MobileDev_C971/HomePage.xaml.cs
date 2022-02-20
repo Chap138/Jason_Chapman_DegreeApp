f19c51783280dfcd6c92e5a24bf7824dc6f7f238
Jason Chapman <jchap71@gmail.com>
2022-02-19 21:12:33 -0800

    mobDevPA

--- Jason_Chapman_MobileDev_C971/HomePage.xaml.cs
+++ Jason_Chapman_MobileDev_C971/HomePage.xaml.cs
@@ -13,20 +13,15 @@ namespace Jason_Chapman_MobileDev_C971
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomePage : ContentPage
     {
-        private string crrntTerm;
+        private int crrntTerm;
         private List<Term> termList;
-        //public event EventHandler Clicked;
-
-        //StackLayout parent = new StackLayout();
         public HomePage()
         {
             InitializeComponent();
             BindingContext = this;
-            //Layout.BindingContext = this;
             //addTermBtnForTest(); //FOR TESTING ///////////////////////
-            //OnAppearing(); Don't need this in Constructor
+            //OnAppearing(); Not needed in Constructor
             addTermFromDB();
-
         }
         protected override void OnAppearing()//Creates all Term buttons from DB info
         {
@@ -35,28 +30,17 @@ namespace Jason_Chapman_MobileDev_C971
 
         private void addTermFromDB()//Creates Buttons for all Terms in DB
         {
-            //Term termTest = new Term()
-            //{
-            //    TermTitle = "Term 1", //Needs to get this from DB because it tests ability to change 'TermTitle'
-            //    CreateDate = System.DateTime.Now
-            //};
-            //int termID = 0;
-            string btnTitle;
-
             using (SQLiteConnection conn = new SQLiteConnection(App.FilePath))
             {
-                //conn.Table<Term>
-                //crrntTerm = termTest.TermID.ToString();
                 termList = conn.Table<Term>().ToList();
                 
                 for (int i = 0; i < termList.Count; i++)
                 {
                     int termID = termList[i].TermID;
-                    btnTitle = termList[i].TermTitle;
+                    string btnTitle = termList[i].TermTitle;
 
                     Button testBtn = new Button()
                     {
-                        //Text = "KJHKg",
                         TextColor = Color.Black,
                         FontAttributes = FontAttributes.Bold,
                         FontSize = 20,
@@ -65,30 +49,13 @@ namespace Jason_Chapman_MobileDev_C971
                     };
 
                     testBtn.Clicked += (sender, args) => GoToTermButton_Clicked(sender, args, termID);
-                    testBtn.BindingContext = termList;
+                    testBtn.BindingContext = termList[i];
                     testBtn.SetBinding(Button.TextProperty, "TermTitle");
 
                     layout.Children.Add(testBtn);
                 }
             }
-
-            //Button testBtn = new Button()
-            //{
-            //    Text = "KJHKg",
-            //    TextColor = Color.Black,
-            //    FontAttributes = FontAttributes.Bold,
-            //    FontSize = 20,
-            //    Margin = 30,
-            //    BackgroundColor = Color.White
-            //};
-
-            //testBtn.Clicked += (sender, args) => GoToTermButton_Clicked(sender, args, termID);
-            //testBtn.BindingContext = termList;
-            //testBtn.SetBinding(Button.TextProperty, "TermTitle");
-
-            //layout.Children.Add(testBtn);
-        }//end addTermBtnFromDB
-
+        }//end addTermFromDB
 
         private void AddTermSaveBtn_Clicked(object sender, EventArgs e)
         {
@@ -127,16 +94,15 @@ namespace Jason_Chapman_MobileDev_C971
 
         private async void GoToTermButton_Clicked(object sender, EventArgs e, int id)//Navigate to appropriate term
         {
-            //await Navigation.PushAsync(new TermPage());
-            
-            await DisplayAlert(id.ToString(), "Werks!!!", "Git!!!");
-
+            await Navigation.PushAsync(new TermPage(crrntTerm));
         }//end GoToTermButton_Clicked
 
-
-
-
-
+        private void AddTermBtn_Clicked(object sender, EventArgs e)//Initiates Term creation 
+        {
+            AddTermEntry.IsVisible = true;
+            AddTermEntry.Focus();
+            AddTermSaveBtn.IsVisible = true;
+        }//end AddTermBtn_Clicked
 
         private void addTermBtnForTest()//FOR TESTING/////////////////////////
         {
@@ -152,7 +118,6 @@ namespace Jason_Chapman_MobileDev_C971
             {
                 conn.CreateTable<Term>();
                 conn.Insert(termTest);
-                crrntTerm = termTest.TermID.ToString();
                 termList = conn.Table<Term>().ToList();
             }
 
@@ -173,14 +138,7 @@ namespace Jason_Chapman_MobileDev_C971
             //int termID = termTest.TermID;
             layout.Children.Add(testBtn);
 
-        }//end addTermBtnForTest FOR TESTING///////////////
-
-        private void AddTermBtn_Clicked(object sender, EventArgs e)//Initiates Term creation 
-        {
-            AddTermEntry.IsVisible = true;
-            AddTermEntry.Focus();
-            AddTermSaveBtn.IsVisible = true;
-        }//end AddTermBtn_Clicked
+        }//end addTermBtnForTest FOR TESTING
         
     }
 }
