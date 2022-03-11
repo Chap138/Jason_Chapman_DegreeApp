4259b10507d8535a61a35ee9f5059a1ca72d4f2e
Jason Chapman <jchap71@gmail.com>
2022-03-10 20:49:11 -0800

    mobDevPa

--- Jason_Chapman_MobileDev_C971/App.xaml.cs
+++ Jason_Chapman_MobileDev_C971/App.xaml.cs
@@ -20,33 +20,37 @@ namespace Jason_Chapman_MobileDev_C971
 
             FilePath = filePath;
 
-            //TestDataForEval();
-        }
+            //using (SQLiteConnection conn = new SQLiteConnection(App.FilePath))
+            //{
+            //    conn.DropTable<Assessment>();
+            //    conn.DropTable<Course>();
+            //    conn.DropTable<Term>();
+            //}
+
+            TestDataForEval();
+        }//end constructor
 
         private void TestDataForEval()
         {
             using (SQLiteConnection conn = new SQLiteConnection(App.FilePath))
             {
-                conn.DeleteAll<Assessment>();
-                conn.DeleteAll<Course>();
-                conn.DeleteAll<Term>();
-
                 conn.CreateTable<Term>();
                 conn.CreateTable<Course>();
                 conn.CreateTable<Assessment>();
 
                 Term termTest = new Term()
                 {
-                    TermTitle = "Term 1",
+                    TermTitle = "Test Term",
                     Start = DateTime.Now,
                     End = DateTime.Now.AddMonths(6)
                 };
-                conn.Insert(termTest);
+                conn.InsertOrReplace(termTest);
+                //conn.Insert(termTest);
 
                 Course courseTest = new Course()
                 {
                     TermID = termTest.ID,
-                    CourseTitle = "Mobile Development",
+                    CourseTitle = "Mobile Development Test",
                     InstructorName = "Jason Chapman",
                     InstructorPhone = "253-736-4078",
                     InstructorEmail = "jchap71@wgu.edu",
@@ -55,25 +59,30 @@ namespace Jason_Chapman_MobileDev_C971
                     EndDate = DateTime.Now.AddMonths(2),
                     CourseNotes = "Mobile development is fun :D"
                 };
-                conn.Insert(courseTest);
+                conn.InsertOrReplace(courseTest);
+                //conn.Insert(courseTest);
 
                 Assessment assmtTest1 = new Assessment()
                 {
                     CourseID = courseTest.CourseID,
-                    AssessmentTitle = "PA 1",
+                    AssessmentTitle = "PA Test",
                     AssessmentType = "Performance Assessment",
                     DueDate = DateTime.Now.AddMonths(1)
                 };
-                conn.Insert(assmtTest1);
+                conn.InsertOrReplace(assmtTest1);
+                //conn.Insert(assmtTest1);
 
                 Assessment assmtTest2 = new Assessment()
                 {
+                    AssessmentID = assmtTest1.AssessmentID + 1,
                     CourseID = courseTest.CourseID,
-                    AssessmentTitle = "OA 1",
+                    AssessmentTitle = "OA Test",
                     AssessmentType = "Objective Assessment",
                     DueDate = DateTime.Now.AddMonths(2)
                 };
-                conn.Insert(assmtTest2);
+
+                conn.InsertOrReplace(assmtTest2);
+                //conn.Insert(assmtTest2);
 
             }
         }//end TestDataForEval
